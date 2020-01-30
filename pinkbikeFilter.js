b55d0d91b401d96ccb79a1d69ea2e415dbe11a37
rwd2 <967059+rwd2@users.noreply.github.com>
2020-01-30 22:06:21 +0100

    Update pinkbikeFilter.js
    cleanup

--- pinkbikeFilter.js
+++ pinkbikeFilter.js
@@ -3,7 +3,7 @@
 // @namespace   Violentmonkey Scripts
 // @include     http://www.pinkbike.com/*
 // @include     https://www.pinkbike.com/*
-// @version     1.1
+// @version     1.2
 // ==/UserScript==
 
 /*** start of settings ***************************************/
@@ -27,24 +27,29 @@ var filterReplies = true;
 // Configure articles  to block
 //
 // If you want to hide front-page articles which have a tag that links to urls
-// 'https://www.pinkbike.com/news/tags/emtb/' or 'https://www.pinkbike.com/news/tags/xc-racing' , 
+// 'https://www.pinkbike.com/news/tags/emtb/' or 'https://www.pinkbike.com/news/tags/xc-racing' ,
 // then use:
-//  
+//
 //     var tags = ['emtb','xc-racing'];
 //
 var tags = [''];
 
 /**** end of settings ****************************************/
 
+var i = 0;
+var ii = 0;
+var iii = 0;
+var articles, tagsInArticle, tagUrl;
+
 // hide articles by tags
 if (articles = document.getElementById('news-container')){
   articles = articles.getElementsByClassName('news-style1');
-  for (var i=0;i < articles.length; i++){
-    var tagsInArticle = articles[i].querySelectorAll('.pb-tag');
-    for (var ii=0;ii<tagsInArticle.length;ii++){
-        var tagUrl = tagsInArticle[ii].getAttribute("href");
-        for (var iii=0;iii<tags.length;iii++){
-           if ( tagUrl == 'https://www.pinkbike.com/news/tags/' + tags[iii]  + '/'){
+  for (i=0;i < articles.length; i++){
+    tagsInArticle = articles[i].querySelectorAll('.pb-tag');
+    for ( ii=0;ii<tagsInArticle.length;ii++){
+        tagUrl = tagsInArticle[ii].getAttribute("href");
+        for ( iii=0;iii<tags.length;iii++){
+           if ( tagUrl == 'https://www.pinkbike.com/news/tags/' + tags[iii] + '/'){
              articles[i].style.display = 'none';
              break;
           }
@@ -56,29 +61,31 @@ if (articles = document.getElementById('news-container')){
 // hide article-comments by username
 
 var arrRegexpAtUserName = [];
-for (var i=0;i<userNames.length;i++){
+for (i=0;i<userNames.length;i++){
   arrRegexpAtUserName[i]= new RegExp('\@'+userNames[i], 'g');
   i++;
- 
+
 }
 
+
+var atUserName, userUrl;
 var collPpcont = document.querySelectorAll('.ppcont');
-for(var i=0;i<collPpcont.length;i++){
+for(i=0;i<collPpcont.length;i++){
   var collCmcont = collPpcont[i].querySelectorAll('.cmcont');
-  for(var ii=0;ii<collCmcont.length;ii++){
+  for( ii=0;ii<collCmcont.length;ii++){
       try{
-        var atUserName = collCmcont[ii].querySelectorAll('.comtext')[0].innerText;
-        var userUrl = collCmcont[ii].children[2].getElementsByTagName('a')[0].getAttribute("href");
+        atUserName = collCmcont[ii].querySelectorAll('.comtext')[0].innerText;
+        userUrl = collCmcont[ii].children[2].getElementsByTagName('a')[0].getAttribute("href");
       }catch(err) {
         console.log(err.message);
       }
-      for (var iii=0;iii<userNames.length;iii++){
-          if  (userUrl && userUrl == 'https://www.pinkbike.com/u/' + userNames[iii] + '/'){
+      for ( iii=0;iii<userNames.length;iii++){
+          if (userUrl && userUrl == 'https://www.pinkbike.com/u/' + userNames[iii] + '/'){
                collCmcont[ii].style.display = 'none';
                if(filterReplies === true && ii==0){
                    collPpcont[i].style.display = 'none';
                }
-               break;            
+               break;
           }
           if ( atUserName && filterReplies === true && atUserName.match(arrRegexpAtUserName[iii])){
              collCmcont[ii].style.display = 'none';
